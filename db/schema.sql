d34ed7ddb8043df123a999026e4edf3fe0445b04
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-07-28 18:11:20 -0600

    Table creatation and comments for clarity

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,45 @@
 -- File for defining the database schema
-
 -- Create department table
+CREATE TABLE department (
+    -- primary key defines the unique id for each department
+    -- Resource: https://www.w3schools.com/sql/sql_primarykey.asp
+    id INT PRIMARY KEY,
+    -- name is the name of the department
+    -- VARCHAR(30) means that the name cannot be more than 30 characters
+    name VARCHAR(30)
+);
 
 -- Create role table
+CREATE TABLE role (
+    -- primary key defines the unique id for each role
+    id INT PRIMARY KEY,
+    -- title is the name of the role
+    title VARCHAR(30),
+    -- salary is the amount of money the role makes
+    -- DECIMAL(10,2) means that the salary can have up to 10 digits, with 2 of them being after the decimal point
+    -- Resource: https://www.w3schools.com/sql/sql_datatypes.asp
+    salary DECIMAL(10,2),
+    -- department_id is the id of the department that the role belongs to
+    department_id INT,
+    -- FOREIGN KEY (department_id) REFERENCES department(id) means that the department_id must match an id in the department table
+    -- Resource: https://www.w3schools.com/sql/sql_foreignkey.asp
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
 -- Create employee table
+CREATE TABLE employee (
+    -- primary key defines the unique id for each employee
+    id INT PRIMARY KEY,
+    -- first_name is the first name of the employee
+    first_name VARCHAR(30),
+    -- last_name is the last name of the employee
+    last_name VARCHAR(30),
+    -- role_id is the id of the role that the employee has
+    role_id INT,
+    -- manager_id is the id of the employee's manager
+    manager_id INT,
+    -- FOREIGN KEY (role_id) REFERENCES role(id) means that the role_id must match an id in the role table
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    -- FOREIGN KEY (manager_id) REFERENCES employee(id) means that the manager_id must match an id in the employee table
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
